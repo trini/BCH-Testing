edc3f0a0ba0c7205d0d3f9895a03ba927e9332b4
Tom Rini <trini@ti.com>
2012-03-13 09:01:41 -0700

    Update the data to not be masked, make tester test a given poly.
    Make tester.c be able to generate the mask used by the NAND driver
    and then check if we have a match for the data.
    
    Signed-off-by: Tom Rini <trini@ti.com>

--- tester.c
+++ tester.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <linux/bch.h>
 
@@ -17,31 +18,63 @@
  * page is a valid codeword' works and what the values being used are.  Must
  * ensure we do that right for the HW equivalent.
  *
+ * In SW matching we take data, run algo, take results and ^ with the matching
+ * part of running our algo over erased (ff) page.  We need to do the same in
+ * our cycling.
+ *
  * XXX: TODO: BIG WARNING
  */
 
-#define ECCSIZE		512
-#define ECCBYTES	13
+#define ECCSIZE			512
+#define ECCBYTES		13
+#define MAX_PRIM_POLY_TEST	8194
 
-extern const uint16_t data[];
-extern const uint16_t ecc_data[];
-
-static uint8_t generated_ecc_data[ECCBYTES];
+extern const uint8_t data[];
+extern const uint8_t ecc_data[];
 
 int main (int argc, char **argv) {
 	struct bch_control *bch = NULL;
 	int i;
+	unsigned int guessed_prim_poly;
+	unsigned char erased_page[ECCSIZE];
+	uint8_t generated_ecc_data[ECCBYTES];
+	uint8_t generated_ecc_mask[ECCBYTES];
 
+	if (argc != 2) {
+		printf("Usage: %s prim_poly\n", argv[0]);
+		exit(1);
+	}
 
-	/* Ensure our buffer is empty */
+	guessed_prim_poly = strtoul(argv[1], NULL, 10);
+	if (!guessed_prim_poly) {
+		printf("Usage: %s prim_poly\n", argv[0]);
+		exit(1);
+	}
+
+	memset(erased_page, 0xff, ECCSIZE);
+
+	/* Ensure our buffer are sane */
 	memset(&generated_ecc_data, 0, sizeof(generated_ecc_data));
+	memset(&generated_ecc_mask, 0, sizeof(generated_ecc_mask));
 
-	bch = init_bch(CONFIG_BCH_CONST_M, CONFIG_BCH_CONST_T, 0);
+	bch = init_bch(CONFIG_BCH_CONST_M, CONFIG_BCH_CONST_T,
+			guessed_prim_poly);
 
-	encode_bch(bch, data, ECCSIZE, generated_ecc_data);
+	/* Get the erased page mask */
+	encode_bch(bch, erased_page, ECCSIZE, generated_ecc_mask);
 	for (i = 0; i < ECCBYTES; i++)
-		printf("0x%x ", generated_ecc_data[i]);
-	printf("\n");
+		generated_ecc_mask[i] ^= 0xff;
 
-	return 0;
+	/* Encode the data */
+	encode_bch(bch, data, ECCSIZE, generated_ecc_data);
+
+	/* Apply the mask */
+	for (i = 0; i < ECCBYTES; i++)
+		generated_ecc_data[i] ^= generated_ecc_mask[i];
+	if (memcmp(generated_ecc_data, ecc_data, ECCBYTES) == 0) {
+		printf("Match with %d\n", guessed_prim_poly);
+		exit(0);
+	}
+
+	return 1;
 }
